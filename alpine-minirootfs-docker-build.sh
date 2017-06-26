bc2645cf926006a44dbab95be15100427ad85a93
Oscar Gómez <oscar.gomez@gmail.com>
2017-06-26 23:29:58 +0200

    Circle Ci last test

--- alpine-minirootfs-docker-build.sh
+++ alpine-minirootfs-docker-build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 rootfsfile=$(head -1 filenames.log)
+echo "Renaming to rootfs.tar.xz"
 cp $rootfsfile rootfs.tar.xz
+echo "Building the Dockerfile"
 docker build .
+echo "Cleaning"
 rm filenames.log
 rm alpine-minirootfs-3*
 
