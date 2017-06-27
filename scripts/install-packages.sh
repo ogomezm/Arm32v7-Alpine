ae163b5f3c6408367e03023384208f0980715d4f
Oscar Gómez <oscar.gomez@gmail.com>
2017-06-27 23:43:22 +0200

    Docker git image

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 apk add --update curl   \
-     qemu-system-x86_64 \
-     qemu-user-static   \
-     binfmt-support
+     qemu-system-x86_64 
 
 addgroup $USER kvm
 rm -rf /var/cache/apk/*
