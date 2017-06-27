538c95b9e44304ba4e4df0bb632db3d8955c6a8d
Oscar Gómez <oscar.gomez@gmail.com>
2017-06-27 02:35:16 +0200

    Circle Ci last test2

--- alpine-dockerhub-build.sh
+++ alpine-dockerhub-build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/ogomez/arm32v7-alpine/trigger/9a51eaac-d7a7-4acb-a335-93c68c87fead/
+let changes=$(git add -v . | wc -l)
+if [$changes > 0]; then
+	git config --global user.email $CIRCLE_USERNAME
+	git config --global user.name "CircleCI"
+	git commit -m "[ci skip] CircleCI rootfs autogeneration"
+	git push origin master
 
+	curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/ogomez/arm32v7-alpine/trigger/9a51eaac-d7a7-4acb-a335-93c68c87fead/
+fi
