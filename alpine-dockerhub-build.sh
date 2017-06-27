d2a1a1a48acef7e9a3f3ba34f7d8fa3b369f3578
Oscar Gómez <oscar.gomez@gmail.com>
2017-06-27 15:50:33 +0200

    Signature validation

--- alpine-dockerhub-build.sh
+++ alpine-dockerhub-build.sh
@@ -5,5 +5,5 @@ if [ "$changes" -gt 0 ]; then
 	git config --global user.name "CircleCI"
 	git commit -m "[ci skip] CircleCI rootfs autogeneration"
 	git push origin master
-	curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/ogomez/arm32v7-alpine/trigger/9a51eaac-d7a7-4acb-a335-93c68c87fead/
+	curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/ogomez/arm32v7-alpine/trigger/$DOCKERHUBKEY/
 fi
